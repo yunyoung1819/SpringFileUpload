975846887f261c3bf3b39b55be5a080b3860612c
Yoon Young <yunyoung1819@hanmail.net>
2018-03-18 23:50:16 +0900

    Spring MVC FileUpload

---
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,23 @@
+package org.zerock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 일반적인 파일 업로드 실습
+ * @author Administrator
+ *
+ */
+@Controller
+public class UploadController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
+	public void uploadForm(){
+		
+	}
+}
