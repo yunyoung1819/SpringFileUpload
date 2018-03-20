c21a3ecbd1f7282e62e3ef801fb3080a87aceed4
Yoon Young <yunyoung1819@hanmail.net>
2018-03-20 23:19:42 +0900

    upload Controller

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -3,8 +3,10 @@ package org.zerock.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 일반적인 파일 업로드 실습
@@ -18,6 +20,11 @@ public class UploadController {
 	
 	@RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
 	public void uploadForm(){
+		
+	}
+	
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
+	public void uploadForm(MultipartFile file, Model model) throws Exception{
 		
 	}
 }
